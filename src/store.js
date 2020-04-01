6818e3e9e47fafe6e30172802bfb932f36721280
pavelkhv <pavel.hvatsik@yandex.by>
2020-04-01 18:16:33 +0300

    added redux

---
+++ src/store.js
@@ -0,0 +1,21 @@
+import { createStore, compose } from 'redux';
+import rootReducer from './reducers/index';
+
+// eslint-disable no-underscore-dangle
+const composeEnhancers =
+  process.env.NODE_ENV !== 'production' &&
+  typeof window === 'object' &&
+  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({}) : compose;
+
+const configureStore = preloadState => (
+  createStore(
+    rootReducer,
+    preloadState,
+    composeEnhancers(),
+  )
+);
+
+const store = configureStore({});
+
+export default store;
